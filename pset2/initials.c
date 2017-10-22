bfb5ae5b82f157d12da10197468f2baf52928cb0
Alex Belokur <alex.belokur17@gmail.com>
2017-10-22 13:13:59 +0300

    resolved task initials from pset2

---
+++ pset2/initials.c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void) 
+{
+    string name = GetString();
+    
+    printf("%c", toupper(name[0]));
+    for (int i = 0, n = strlen(name); i < n; i++) 
+    {
+        if(isspace(name[i-1]))
+        printf("%c", toupper(name[i]));
+    }
+    printf("\n");
+}
